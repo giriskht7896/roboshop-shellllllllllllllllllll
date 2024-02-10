b2deb8c996874472aab11e13579097f46030f78b
Gireesh <gireesh@DevOps@gmail.com>
2024-02-10 11:49:34 +0530

    create ec2 instances

---
+++ roboshop-shell/create-ec2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+NAMES=( "mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment"  "web")
+
+IMAGE_ID=ami-0f3c7d07486cad139
+SECURITY_GROUP_ID=sg-0d2c442d103270735 
+INSTANCE_TYPE=""
+DOMAIN_NAME=devopsskht.xyz
+
+for i in "${NAMES[@]}"
+do
+    if [[ $i == "mongodb" || $i == "mysql" ]];
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro" 
+    fi
+
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$i}]' | jq -r '.Instance[0].PrivateIpAddress')
+
+    echo "created $i instance: $IP_ADDRESS"
+
+     aws route53 change-resource-record-sets --hosted-zone-id Z01278211VH22AL56K3B8 --change-batch '
+    {"Changes": [ { 
+        "Action": "CREATE",
+         "ResourceRecordSet": {
+             "Name": "$i.$DOMAIN_NAME", 
+             "Type": "A", 
+             "TTL": 3600, 
+             "ResourceRecords": [{ "Value": "$IP_ADDRESS" }] 
+             } } 
+             ]
+    }
+    '
+
+done
+
+
+
+
+
+
